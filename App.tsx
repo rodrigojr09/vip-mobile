22cf402452e9a89e91700f32689c8befe15bda6c
Muriloferreiraa <155457001+Muriloferreiraa@users.noreply.github.com>
2025-01-16 16:00:49 -0300

    rodrigo

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Header */}
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Vip Mobile</Text>
+      </View>
+
+      {/* Botões */}
+      <View style={styles.body}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}> Novo Levantamento </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}> Medição de Calor </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -14,7 +26,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  header: {
+    backgroundColor: 'green',
+    padding: 37,
     alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 34,
+    fontWeight: 'bold',
+  },
+  body: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 30, 
+  },
+  button: {
+    backgroundColor: 'green',
+    paddingVertical: 25,
+    paddingHorizontal: 40,
+    marginVertical: 14, 
+    borderRadius: 10,
+    width: '96%', 
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 24,
+    textAlign: 'center',
   },
 });
